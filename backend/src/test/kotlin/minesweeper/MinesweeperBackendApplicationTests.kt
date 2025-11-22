501aba39824398e927d0ca1f39123c83763ab2d1
Josefu <yusuf.birdane@stud.h-da.de>
2025-11-23 00:15:56 +0100

    first commit

---
+++ backend/src/test/kotlin/minesweeper/MinesweeperBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package minesweeper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MinesweeperBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
